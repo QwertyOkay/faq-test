5f316e955c45941a4c7c25d1822b9d20e6d11c62
Alexandr Kotenko <kpshmt@gmail.com>
2023-11-02 21:43:21 +0200

    buttons update

--- js/faq.js
+++ js/faq.js
@@ -1,34 +1,24 @@
-// const items = document.querySelectorAll(".accordion button");
+ document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".accordion button");
 
-// function toggleAccordion() {
-//   const itemToggle = this.getAttribute('aria-expanded');
-  
-//   for (i = 0; i < items.length; i++) {
-//     items[i].setAttribute('aria-expanded', 'false');
-//   }
-  
-//   if (itemToggle == 'false') {
-//     this.setAttribute('aria-expanded', 'true');
-//   }
-// }
+  function toggleAccordion() {
+    
+    const iconPlus = this.querySelector('.icon.plus');
+    const iconMinus = this.querySelector('.icon.minus');
+    const isExpanded = this.getAttribute("aria-expanded") === "true";
 
-// items.forEach(item => item.addEventListener('click', toggleAccordion));
-
-const items = document.querySelectorAll(".accordion button");
-
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  const arrowIcon = this.querySelector('.icon');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-    items[i].querySelector('.icon').classList.remove('active');
+    if (isExpanded) {
+      this.setAttribute("aria-expanded", "false");
+      iconPlus.style.display = "inline"; 
+      iconMinus.style.display = "none"; 
+    } else {
+      this.setAttribute("aria-expanded", "true");
+      iconPlus.style.display = "none"; 
+      iconMinus.style.display = "inline"; 
+    }
   }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-    arrowIcon.classList.add('active');
-  }
-}
 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+  buttons.forEach((button) => {
+    button.addEventListener("click", toggleAccordion);
+  });
+});

d65b1a4b8d1d5100c676652ec374b0f0108a9b17
Alexandr Kotenko <kpshmt@gmail.com>
2023-11-02 20:13:13 +0200

    update

--- js/faq.js
+++ js/faq.js
@@ -1,14 +1,33 @@
+// const items = document.querySelectorAll(".accordion button");
+
+// function toggleAccordion() {
+//   const itemToggle = this.getAttribute('aria-expanded');
+  
+//   for (i = 0; i < items.length; i++) {
+//     items[i].setAttribute('aria-expanded', 'false');
+//   }
+  
+//   if (itemToggle == 'false') {
+//     this.setAttribute('aria-expanded', 'true');
+//   }
+// }
+
+// items.forEach(item => item.addEventListener('click', toggleAccordion));
+
 const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute('aria-expanded');
+  const arrowIcon = this.querySelector('.icon');
   
   for (i = 0; i < items.length; i++) {
     items[i].setAttribute('aria-expanded', 'false');
+    items[i].querySelector('.icon').classList.remove('active');
   }
   
   if (itemToggle == 'false') {
     this.setAttribute('aria-expanded', 'true');
+    arrowIcon.classList.add('active');
   }
 }
 

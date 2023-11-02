1df551dfa7308addf4dbc1ef2d14b6978390bb9d
Alexandr Kotenko <kpshmt@gmail.com>
2023-11-02 22:41:07 +0200

    add animation

--- js/faq.js
+++ js/faq.js
@@ -1,24 +1,41 @@
- document.addEventListener("DOMContentLoaded", function () {
-  const buttons = document.querySelectorAll(".accordion button");
+//  document.addEventListener("DOMContentLoaded", function () {
+//   const buttons = document.querySelectorAll(".accordion button");
 
-  function toggleAccordion() {
+//   function toggleAccordion() {
     
-    const iconPlus = this.querySelector('.icon.plus');
-    const iconMinus = this.querySelector('.icon.minus');
-    const isExpanded = this.getAttribute("aria-expanded") === "true";
+//     const iconPlus = this.querySelector('.icon.plus');
+//     const iconMinus = this.querySelector('.icon.minus');
+//     const isExpanded = this.getAttribute("aria-expanded") === "true";
 
-    if (isExpanded) {
-      this.setAttribute("aria-expanded", "false");
-      iconPlus.style.display = "inline"; 
-      iconMinus.style.display = "none"; 
-    } else {
-      this.setAttribute("aria-expanded", "true");
-      iconPlus.style.display = "none"; 
-      iconMinus.style.display = "inline"; 
-    }
+//     if (isExpanded) {
+//       this.setAttribute("aria-expanded", "false");
+//       iconPlus.style.display = "inline"; 
+//       iconMinus.style.display = "none"; 
+//     } else {
+//       this.setAttribute("aria-expanded", "true");
+//       iconPlus.style.display = "none"; 
+//       iconMinus.style.display = "inline"; 
+//     }
+//   }
+
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", toggleAccordion);
+//   });
+// });
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
   }
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", toggleAccordion);
-  });
-});
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
